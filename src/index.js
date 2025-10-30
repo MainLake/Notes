72ba12bee454129ec715457af31b780af331adf1
MainLake <leonmaing@gmail.com>
2025-10-30 16:50:18 -0600

    fix: Correct invalid route pattern in production server

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
-  app.get('*_*, (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 } else {
