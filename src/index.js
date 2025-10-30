cacfba39fb47fc5ee15f68d2e60e73b902d73d59
MainLake <leonmaing@gmail.com>
2025-10-30 16:54:47 -0600

    fix: Correct path to production frontend assets

--- src/index.js
+++ src/index.js
@@ -24,12 +24,12 @@ app.use('/api/tasks', taskRoutes);
 // Production Frontend Serving Logic
 if (process.env.NODE_ENV === 'production') {
   // Serve static files from the React build directory
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 } else {
   // Development-only welcome message
